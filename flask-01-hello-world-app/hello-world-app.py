c871e034703fcc6da8810bcee1dfc2ba5f83a411
kartal <yasinkartal2009@gmail.com>
2023-08-22 13:58:08 +0300

    flask push

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -1,8 +1,21 @@
 from flask import Flask 
 app = Flask(__name__)
 
+@app.route('/')
+def head():
+    return "Hello cohort 15"
 
+@app.route('/second')
+def second():
+    return "This is second page"
 
+@app.route('/third')
+def third():
+    return "This is third page"
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
 
 
 
--- flask-02-Jinja_Template/jinja.py
+++ flask-02-Jinja_Template/jinja.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/")
+def head():
+    return render_template("index.html", number1=1234, number2=78)
 
-
-
-
+@app.route("/cohort15")
+def number():
+    num1 = 12
+    num2 = 10
+    return render_template("body.html", value1=num1, value2=num2, sum=num1+num2)
 
 
 
